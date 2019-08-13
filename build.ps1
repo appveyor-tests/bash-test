61698d8fe1c68358e26a0a43bd73622885763e3a
Alena Hrynkevich <alena@appveyor.com>
2019-08-13 11:59:09 -0700

    " -> '

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $sh_mapping = (cmd /c ftype sh_auto_file)
 $sh_mapping
 if ($sh_mapping -ne 'sh_auto_file=C:\msys64\usr\bin\bash.exe "%L" %*') { throw ".sh mapping is not set."; }
-$text = "echo "Hello, world!""
+$text = "echo 'Hello, world!'"
 $text > $env:temp\test.sh
 $sh_result = (echo cmd /c $env:temp\test.sh)
 $sh_result
