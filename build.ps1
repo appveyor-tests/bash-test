da27e5e544b9ad4fd3f1e0f81795a97ae5e7860d
Alena Hrynkevich <alena@appveyor.com>
2019-08-13 15:17:13 -0700

    Print content of newly created test.sh

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ $sh_mapping = (cmd /c ftype sh_auto_file)
 $sh_mapping
 if ($sh_mapping -ne 'sh_auto_file=C:\msys64\usr\bin\bash.exe "%L" %*') { throw ".sh mapping is not set."; }
 cmd /c echo echo "Hello, world!" > $env:temp\test.sh
+Get-Content -Path $env:temp\test.sh
 $sh_result = (echo cmd /c $env:temp\test.sh)
 $sh_result
 if ($sh_result -ne 'Hello, world!') { throw "Error running .sh script."; }
