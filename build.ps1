98ed0092e93c9d7cf625b5f8c1306ac1769c9783
Alena Hrynkevich <alena@appveyor.com>
2019-08-12 18:06:11 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 $sh_mapping = (cmd /c ftype sh_auto_file)
 $sh_mapping
 if ($sh_mapping -ne 'sh_auto_file=C:\msys64\usr\bin\bash.exe "%L" %*') { throw ".sh mapping is not set."; }
-echo "Hello, world!" > %temp%\test.sh
+echo "Hello, world!" > $env:temp\test.sh
 $sh_result = (cmd /c $env:temp\test.sh)
 $sh_result
 if ($sh_result -ne 'Hello, world!') { throw "Error running .sh script."; }
