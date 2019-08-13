ae06303ee9e1bc769792c4de12d4289c296390ef
Alena Hrynkevich <alena@appveyor.com>
2019-08-13 15:19:37 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@ $sh_mapping = (cmd /c ftype sh_auto_file)
 $sh_mapping
 if ($sh_mapping -ne 'sh_auto_file=C:\msys64\usr\bin\bash.exe "%L" %*') { throw ".sh mapping is not set."; }
 cmd /c echo echo "Hello, world!" > $env:temp\test.sh
+Write-Host "test.sh content: "
 Get-Content -Path $env:temp\test.sh
 $sh_result = (echo cmd /c $env:temp\test.sh)
 $sh_result
