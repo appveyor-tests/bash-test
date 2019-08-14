de3cc513a788824c27d4fb03b3a176a47085ddf5
Alena Hrynkevich <alena@appveyor.com>
2019-08-14 10:14:02 -0700

    Try running bash script

--- build.ps1
+++ build.ps1
@@ -4,6 +4,8 @@ if ($sh_mapping -ne 'sh_auto_file=C:\msys64\usr\bin\bash.exe "%L" %*') { throw "
 cmd /c echo echo "Hello, world!" > $env:temp\test.sh
 Write-Host "test.sh content: "
 Get-Content -Path $env:temp\test.sh
+#try running test.sh
+cmd /c $env:temp\test.sh
 $sh_result = (echo cmd /c $env:temp\test.sh)
 $sh_result
 if ($sh_result -ne 'Hello, world!') { throw "Error running .sh script."; }
